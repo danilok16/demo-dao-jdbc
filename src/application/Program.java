2fe878b409d7d99b9278263467ec5e941ad35337
Danilo Alves <32230794+danilok16@users.noreply.github.com>
2019-03-12 15:21:00 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		/*
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
@@ -32,16 +32,20 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 3: seller insert ===");
+		System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 4: seller update ===");
+		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		*/
+		System.out.println("\n=== TEST 6: seller delete ===");
+		sellerDao.deleById(11);
+		System.out.println("Delete completed");
 	}
 
 }
